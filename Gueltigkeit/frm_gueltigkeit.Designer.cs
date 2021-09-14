ab806ad63fe515a4d95436c0cfe700eb1a83ac1b
Dirk Burggraf <dirk.aus.dresden@gmx.de>
2021-09-14 12:35:45 +0200

    Projektdateien hinzufügen.

---
+++ Gueltigkeit/frm_gueltigkeit.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Gueltigkeit
+{
+    partial class frm_gueltigkeit
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_gueltigkeit_1 = new System.Windows.Forms.Button();
+            this.btn_gueltigkeit_2 = new System.Windows.Forms.Button();
+            this.lbl_gueltigkeit_1 = new System.Windows.Forms.Label();
+            this.lbl_gueltigkeit_2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_gueltigkeit_1
+            // 
+            this.btn_gueltigkeit_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_gueltigkeit_1.Location = new System.Drawing.Point(12, 111);
+            this.btn_gueltigkeit_1.Name = "btn_gueltigkeit_1";
+            this.btn_gueltigkeit_1.Size = new System.Drawing.Size(395, 67);
+            this.btn_gueltigkeit_1.TabIndex = 0;
+            this.btn_gueltigkeit_1.Text = "Anzeige Gültigkeitsbereich 1";
+            this.btn_gueltigkeit_1.UseVisualStyleBackColor = true;
+            this.btn_gueltigkeit_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_gueltigkeit_2
+            // 
+            this.btn_gueltigkeit_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_gueltigkeit_2.Location = new System.Drawing.Point(12, 196);
+            this.btn_gueltigkeit_2.Name = "btn_gueltigkeit_2";
+            this.btn_gueltigkeit_2.Size = new System.Drawing.Size(395, 67);
+            this.btn_gueltigkeit_2.TabIndex = 1;
+            this.btn_gueltigkeit_2.Text = "Anzeige Gültigkeitsbereich 2";
+            this.btn_gueltigkeit_2.UseVisualStyleBackColor = true;
+            this.btn_gueltigkeit_2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbl_gueltigkeit_1
+            // 
+            this.lbl_gueltigkeit_1.AutoSize = true;
+            this.lbl_gueltigkeit_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_gueltigkeit_1.Location = new System.Drawing.Point(448, 128);
+            this.lbl_gueltigkeit_1.Name = "lbl_gueltigkeit_1";
+            this.lbl_gueltigkeit_1.Size = new System.Drawing.Size(321, 26);
+            this.lbl_gueltigkeit_1.TabIndex = 2;
+            this.lbl_gueltigkeit_1.Text = "Hier steht gleich etwas anderes!";
+            // 
+            // lbl_gueltigkeit_2
+            // 
+            this.lbl_gueltigkeit_2.AutoSize = true;
+            this.lbl_gueltigkeit_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_gueltigkeit_2.Location = new System.Drawing.Point(448, 216);
+            this.lbl_gueltigkeit_2.Name = "lbl_gueltigkeit_2";
+            this.lbl_gueltigkeit_2.Size = new System.Drawing.Size(321, 26);
+            this.lbl_gueltigkeit_2.TabIndex = 3;
+            this.lbl_gueltigkeit_2.Text = "Hier steht gleich etwas anderes!";
+            // 
+            // frm_gueltigkeit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_gueltigkeit_2);
+            this.Controls.Add(this.lbl_gueltigkeit_1);
+            this.Controls.Add(this.btn_gueltigkeit_2);
+            this.Controls.Add(this.btn_gueltigkeit_1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frm_gueltigkeit";
+            this.Text = "Gueltigkeitsbereich von Variablen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_gueltigkeit_1;
+        private System.Windows.Forms.Button btn_gueltigkeit_2;
+        private System.Windows.Forms.Label lbl_gueltigkeit_1;
+        private System.Windows.Forms.Label lbl_gueltigkeit_2;
+    }
+}
+
---
+++ Gueltigkeit/frm_gueltigkeit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gueltigkeit
+{
+    public partial class frm_gueltigkeit : Form
+    {
+        // Deklaration einer Variable mit dem Namen Zaehler
+        // dieser wird der Wert 0 zugewiesen
+        // Variable hat den Datentyp int
+        // private besagt, dass Variable innerhalb der Klasse gültig ist, also benutzt werden kann
+        // die Variable behält ihren Wert auch wenn Methoden verlassen werden
+        private int Zaehler = 0;
+
+        public frm_gueltigkeit()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Zaehler um 1 erhöhen
+            Zaehler++;
+
+            // ebenso möglich
+            // der neue Wert von Zaehler ist gleich der alte Wert + 1
+            Zaehler = Zaehler + 1;
+
+            // lbl_gueltigkeit_1.Text ist vom Datentyp string
+            // Zaehler ist vom Datentyp int
+            // Konvertierung von int in string mit Hilfe der Methode ToString()
+            lbl_gueltigkeit_1.Text = Zaehler.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Variablen, die in der Methode deklariert und zugewiesen werden, sind nur innerhalb der Methode gültig
+            // wird die Methode verlassen, ist der Wert der Variablen weg
+            int Zaehler_lokal = 0;
+
+            Zaehler_lokal++;
+
+            Zaehler_lokal = Zaehler_lokal + 1;
+
+            lbl_gueltigkeit_2.Text = Zaehler_lokal.ToString();
+        }
+    }
+}
